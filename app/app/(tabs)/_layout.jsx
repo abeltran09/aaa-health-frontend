20f69960c877f6bb72c169a4e678bc30f1e3ec06
Angel Beltran <aabeltran5910@gmail.com>
2024-11-12 13:54:38 -0600

    login and nav added

---
+++ app/app/(tabs)/_layout.jsx
@@ -0,0 +1,52 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+
+export default function AppLayout() {
+  const colorScheme = 'light'
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: Colors[colorScheme].tint
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon 
+            name={focused ? 'home' : 'home-outline'} 
+            color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="dashboard"
+        options={{
+          title: 'Dashboards',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon 
+            name={focused ? 'analytics' : 'analytics-outline'} 
+            color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon 
+            name={focused ? 'settings' : 'settings-outline'} 
+            color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/app/(tabs)/index.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform} from 'react-native';
+
+export default function HomeScreen() {
+    return (
+
+        <Text>Hello</Text>
+    );
+
+}
